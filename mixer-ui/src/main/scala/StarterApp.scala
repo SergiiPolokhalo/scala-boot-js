a4c66447e688f7e3ee8568003c36eff0cb6a42b8
Serhii Polokhalo <kitsonkit@gmail.com>
2018-04-29 22:56:55 -0400

    fix ui

---
+++ mixer-ui/src/main/scala/StarterApp.scala
@@ -0,0 +1,21 @@
+package com.example
+
+import scala.scalajs.js
+import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+object StarterApp {
+  var count = 0
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(callback: js.Dynamic): Unit = {
+
+    println("clicked")
+
+    count += 1
+
+    if(count>4){
+      callback("bleep"+count)
+    }
+    callback(count)
+  }
+}
